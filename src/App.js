8d5ca93042b390fe0c5fd800ef1de3bb0928226c
romanbro <82284311+Roman123-game@users.noreply.github.com>
2024-06-19 12:28:30 +0300

    update 5 files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+import { useGetPokemonByNameQuery } from './services/pokemon'
 
-function App() {
+export default function App() {
+  // Using a query hook automatically fetches data and returns query values
+  const { data, error, isLoading } = useGetPokemonByNameQuery()
+  console.log(data)
+  // Individual hooks are also accessible under the generated endpoints:
+  // const { data, error, isLoading } = pokemonApi.endpoints.getPokemonByName.useQuery('bulbasaur')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          {data.results.map(dat => (
+            <p className='data' key={dat.name}>
+              <h3  >{dat.name}</h3>
+              {/* <img src={dat.sprites.front_shiny} alt={data.species.name} /> */}
+            </p>
+          ))}
 
-export default App;
+        </>
+      ) : null}
+    </div>
+  )
+}
