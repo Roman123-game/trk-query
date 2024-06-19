d1ab1f67fd5ca2803442b6e435b48516918ec0cb
romanbro <82284311+Roman123-game@users.noreply.github.com>
2024-06-19 13:48:47 +0300

    update App.js and store.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import * as React from 'react'
-import { useGetPokemonByNameQuery } from './services/pokemon'
+// import { useGetPokemonByNameQuery } from './services/pokemon'
+import { useGetpostsByNameQuery }  from './services/posts'
+
 
 export default function App() {
   // Using a query hook automatically fetches data and returns query values
-  const { data, error, isLoading } = useGetPokemonByNameQuery()
+  const { data, error, isLoading } = useGetpostsByNameQuery("")
   console.log(data)
   // Individual hooks are also accessible under the generated endpoints:
   // const { data, error, isLoading } = pokemonApi.endpoints.getPokemonByName.useQuery('bulbasaur')
@@ -16,11 +18,11 @@ export default function App() {
         <>Loading...</>
       ) : data ? (
         <>
-          {data.results.map(dat => (
-            <p className='data' key={dat.name}>
-              <h3  >{dat.name}</h3>
+          {data.map(dat => (
+            <div className='data' key={dat.id}>
+              <h3  >{dat.body}</h3>
               {/* <img src={dat.sprites.front_shiny} alt={data.species.name} /> */}
-            </p>
+            </div>
           ))}
 
         </>
--- src/store.js
+++ src/store.js
@@ -1,17 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 // Or from '@reduxjs/toolkit/query/react'
 import { setupListeners } from '@reduxjs/toolkit/query'
-import { pokemonApi } from './services/pokemon'
+// import { pokemonApi } from './services/pokemon'
+import { postsApi } from './services/posts'
 
 export const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
-    [pokemonApi.reducerPath]: pokemonApi.reducer,
+    // [pokemonApi.reducerPath]: pokemonApi.reducer,
+   [postsApi.reducerPath]: postsApi.reducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(pokemonApi.middleware),
+    getDefaultMiddleware().concat(postsApi.middleware),
 })
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
